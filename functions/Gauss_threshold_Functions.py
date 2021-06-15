3025de48f3c6454497e3adbf371f8757f6c08126
Enn Veikesaar <ennveik@hotmail.com>
2021-06-15 21:39:38 +0100

    Final
    Comments more precise.

--- functions/Gauss_threshold_Functions.py
+++ functions/Gauss_threshold_Functions.py
@@ -12,7 +12,7 @@ def Gaussian_thresholding(path):
     i = 1 #Integer to keep track of the used images in the while loop
     imagelist = [] #List to contain all the processed images   
     pure_imagelist = [] #List to contain all the unprocessed images
-    while i <= image_count(path): #16 images, i values up to and including 16
+    while i <= image_count(path): #Use the image counting function to set the limits for the while loop
         
         image = cv2.imread(path + "chu" + str(i) + ".jpg", 0) #Reads in the image. Do not change the original filenames.
         image_threshold =  cv2.adaptiveThreshold(image, 255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 7, 1) #Conduct adaptive Gaussian Thresholding on the image
